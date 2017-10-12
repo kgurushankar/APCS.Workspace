3ab4d7f14cc8db68156f6497b07e2e14d881f97a
kgurushankar <keshavgurushankar@hotmail.com>
2017-10-11 23:49:29 -0700

    mini fix

--- Google/src/com/google/challenges/level1/Answer.java
+++ Google/src/com/google/challenges/level1/Answer.java
@@ -6,6 +6,7 @@ public class Answer {
 	public static void main(String args[]) {
 		Scanner in = new Scanner(System.in);
 		System.out.println(answer(in.nextLine()));
+		in.close();
 	}
 
 	public static int answer(String s) {
@@ -13,7 +14,7 @@ public class Answer {
 		for (int i = test.length - 1; i >= 0; i--) {
 			String[] pieces = new String[test[i]];
 			for (int j = 0; j < pieces.length; j++) {
-				pieces[j] = s.substring((j) * (s.length() / test[i]), (j + 1) * (s.length() / test[i]) );
+				pieces[j] = s.substring((j) * (s.length() / test[i]), (j + 1) * (s.length() / test[i]));
 			}
 			boolean clear = true;
 			for (int j = 1; j < pieces.length; j++) {
