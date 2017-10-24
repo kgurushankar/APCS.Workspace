b22d963e2fd2f05b7244db3ee20e7c083945c0b5
kgurushankar <keshavgurushankar@hotmail.com>
2017-10-24 09:44:57 -0700

    Finish Tester

--- CheckMail/src/kgurushankar/checkmail/Main.java
+++ CheckMail/src/kgurushankar/checkmail/Main.java
@@ -5,10 +5,17 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println(new Package(15,20,25,80).firstClass());
 		Scanner in = new Scanner(System.in);
 		System.out.print("Enter 1st Dimension");
-		int dim1 = 
+		int dim1 = in.nextInt();
+		System.out.print("Enter 2nd Dimension");
+		int dim2 = in.nextInt();
+		System.out.print("Enter 3rd Dimension");
+		int dim3 = in.nextInt();
+		System.out.print("Enter Weight");
+		int weight = in.nextInt();
+		in.close();
+		System.out.println(new Package(dim1, dim2, dim3, weight).firstClass());
 	}
 
 }
