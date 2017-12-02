7453d61dce684db27f3a834b08c57047fef9dc5c
Unknown <keshavgurushankar@hotmail.com>
2017-12-01 22:06:04 -0800

    Formatting and minor changes

--- Resizable Array/src/kgurushankar/arraystatistics/ResizableArray.java
+++ Resizable Array/src/kgurushankar/arraystatistics/ResizableArray.java
@@ -2,6 +2,13 @@ package kgurushankar.arraystatistics;
 
 import java.util.Arrays;
 
+// Add, Remove, size, removeAll, insert, getData, set,
+// replace, clear, remove range, subset, sort, indexOf,
+
+// replaceall, reverse, remove range, subarray, sort, index of, insert, size,
+// clear, addAll, remove
+
+//BTW index cannot be <0
 public class ResizableArray {
 	private int data[];
 	private int size;
@@ -21,115 +28,29 @@ public class ResizableArray {
 		add(value);
 	}
 
-	public void insert(int index, int[] value) {
-		if (index > size) {
-			throw new IllegalArgumentException("Insertion location must be within array");
-		} else if (size + value.length > data.length) {
-			throw new IndexOutOfBoundsException("Insufficent Space in array");
-		}
-
-		for (int i = size - 1; i <= index; i++) {
-			data[i + value.length] = data[i];
-		}
-		for (int i = 0; i < value.length; i++) {
-			data[i + index] = value[i];
-		}
-		size += value.length;
+	public ResizableArray(ResizableArray other) {
+		this();
+		add(other.toArray());
+		this.size = other.size;
 	}
 
-	public void insert(int index, int value) {
-		insert(index, new int[] { value });
-	}
-
-	// Add, Remove, getSize, removeAll, insert, getData, set,
-	// replace, clear, remove range, subset, sort, indexOf,
-
-	// replaceall, reverse, remove range, subarray, sort, index of, insert, size,
-	// clear, addAll, remove
-
-	public int removeAll(int bad) {
-		int bads = 0;
-		for (int i = 0; i < size; i++) {
-			if (data[i] == bad) {
-				bads++;
-			} else {
-				data[i - bads] = data[i];
-			}
-		}
-		size -= bads;
-		return size;
-	}
-
-	public int add(int value) {
+	public void add(int value) {
 		data[size] = value;
 		size++;
-		return size;
 	}
 
-	public int add(int[] values) {
+	public void add(int[] values) {
 		for (int val : values) {
 			add(val);
 		}
-		return size;
+	}
+
+	public void add(ResizableArray other) {
+		add(other.toArray());
 	}
 
 	public void clear() {
 		size = 0;
-	}
-
-	public void replaceAll(int Old, int New) {
-		for (int i = 0; i < size; i++)
-			if (data[i] == Old)
-				data[i] = New;
-	}
-
-	public int size() {
-		return size;
-	}
-
-	public int remove(int index) {
-		if (index >= size) {
-			throw new IndexOutOfBoundsException("Index:" + index + " Size:" + size);
-		}
-		int out = data[index];
-		for (int i = index; i < size - 1; i++) {
-			data[i] = data[i + 1];
-		}
-		size--;
-		return out;
-	}
-
-	public String toString() {
-		if (size == 0) {
-			return "[]";
-		}
-		String out = "[";
-		for (int i = 0; i < size - 1; i++) {
-			out += data[i] + ", ";
-		}
-		out += data[size - 1] + "]";
-		return out;
-	}
-
-	public void set(int index, int value) {
-		data[index] = value;
-	}
-
-	public int indexOf(int value) {
-		for (int i = 0; i < size; i++) {
-			if (data[i] == value) {
-				return i;
-			}
-		}
-		return -1;
-	}
-
-	public int[] toArray() {
-		int[] out = new int[size];
-		for (int i = 0; i < out.length; i++) {
-			out[i] = this.data[i];
-		}
-		return out;
 	}
 
 	public boolean equals(Object obj) {
@@ -146,14 +67,138 @@ public class ResizableArray {
 		return true;
 	}
 
-	public void sort() {
-		Arrays.sort(this.data, 0, size);
-	}
-
 	public int get(int index) {
 		if (index >= size) {
 			throw new IndexOutOfBoundsException("Index:" + index + " Size:" + size);
 		}
 		return data[index];
 	}
+
+	public int indexOf(int value) {
+		for (int i = 0; i < size; i++) {
+			if (data[i] == value) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public void insert(int index, int value) {
+		insert(index, new int[] { value });
+	}
+
+	public void insert(int index, int[] value) {
+		if (index > size) {
+			throw new IllegalArgumentException("Insertion location must be within array");
+		} else if (size + value.length > data.length) {
+			throw new IndexOutOfBoundsException("Insufficent Space in array");
+		}
+
+		for (int i = size - 1; i <= index; i++) {
+			data[i + value.length] = data[i];
+		}
+		for (int i = 0; i < value.length; i++) {
+			data[i + index] = value[i];
+		}
+		size += value.length;
+	}
+
+	public int remove(int index) {
+		if (index >= size) {
+			throw new IndexOutOfBoundsException("Index:" + index + " Size:" + size);
+		}
+		int out = data[index];
+		for (int i = index; i < size - 1; i++) {
+			data[i] = data[i + 1];
+		}
+		size--;
+		return out;
+	}
+
+	// TODO finish
+	public int[] remove(int start, int end) {
+		int len = end - start;
+		if (end >= size) {
+			throw new IndexOutOfBoundsException("Index:" + end + " Size:" + size);
+		}
+		int out[] = new int[len];
+		for (int i = 0; i < len; i++) {
+			out[i] = data[start + i];
+		}
+		for (int i = start; i < size - len; i++) {
+			data[i] = data[i + len];
+		}
+		size -= len;
+		return out;
+
+	}
+
+	public int removeAll(int value) {
+		int bads = 0;
+		for (int i = 0; i < size; i++) {
+			if (data[i] == value) {
+				bads++;
+			} else {
+				data[i - bads] = data[i];
+			}
+		}
+		size -= bads;
+		return size;
+	}
+
+	public void replaceAll(int Old, int New) {
+		for (int i = 0; i < size; i++)
+			if (data[i] == Old)
+				data[i] = New;
+	}
+
+	public void reverse() {
+		int temp;
+		for (int i = 0; i < (size / 2); ++i) {
+			temp = data[i];
+			data[i] = data[size - i - 1];
+			data[size - i - 1] = temp;
+		}
+	}
+
+	public void set(int index, int value) {
+		data[index] = value;
+	}
+
+	public int size() {
+		return size;
+	}
+
+	public void sort() {
+		Arrays.sort(this.data, 0, size);
+	}
+
+	public ResizableArray subset(int start, int end) {
+		ResizableArray out = new ResizableArray();
+		for (int i = start; i < end; i++) {
+			out.add(i);
+		}
+		return out;
+	}
+
+	public int[] toArray() {
+		int[] out = new int[size];
+		for (int i = 0; i < out.length; i++) {
+			out[i] = this.data[i];
+		}
+		return out;
+	}
+
+	public String toString() {
+		if (size == 0) {
+			return "[]";
+		}
+		String out = "[";
+		for (int i = 0; i < size - 1; i++) {
+			out += data[i] + ", ";
+		}
+		out += data[size - 1] + "]";
+		return out;
+	}
+
 }
