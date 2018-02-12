1afa24f4b645e4c02a4438332056c9cef8c3ed2d
Unknown <keshavgurushankar@hotmail.com>
2018-02-12 11:17:20 -0800

    Tree Fractal

--- Fractal/src/kgurushankar/fractal/tree/Curve.java
+++ Fractal/src/kgurushankar/fractal/tree/Curve.java
@@ -6,15 +6,12 @@ import java.util.ArrayList;
 import kgurushankar.fractal.Line;
 
 public class Curve extends kgurushankar.fractal.Curve {
-
 	public Curve(int level, int x, int y, int x2, int y2) {
 		super(level, x, y, x2, y2);
-		// TODO Auto-generated constructor stub
 	}
 
 	public Curve(int level, Point start, Point end) {
 		super(level, start, end);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
