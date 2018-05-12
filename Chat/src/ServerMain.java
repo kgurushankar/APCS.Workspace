1ed6569e6391aeb848f2f6073803f6d0e0232608
Unknown <keshavgurushankar@hotmail.com>
2018-05-11 21:40:12 -0700

    Cleanup of Chat

--- Chat/src/ServerMain.java
+++ Chat/src/ServerMain.java
@@ -2,15 +2,10 @@ import java.io.IOException;
 
 public class ServerMain {
 	public static void main(String[] args) {
-		Server s = null;
 		try {
-			s = new Server(8888);
+			@SuppressWarnings({ "unused", "resource" }) // I need this to stay open, and all processing happens internally
+			Server s = new Server(8888);
 		} catch (IOException e0) {
-			// try {
-			// s.close();
-			// } catch (NullPointerException e1) {
-			// } catch (IOException e) {
-			// }
 			e0.printStackTrace(System.out);
 			System.exit(0);
 		}
